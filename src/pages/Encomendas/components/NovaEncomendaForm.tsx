c8cf634781b07e82cfd0990da8acd6ce6364bc9c
Pedro Aurélio <pedroaurelio.reis@gmail.com>
2026-09-01 16:56:24 -0300

    fix(ui): corrigir importacao do axios api que causava tela branca

--- src/pages/Encomendas/components/NovaEncomendaForm.tsx
+++ src/pages/Encomendas/components/NovaEncomendaForm.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Package, User, MapPin, Truck } from 'lucide-react';
-import { api } from '../../../services/api';
+import api from '../../../services/api';
 
 interface NovaEncomendaFormProps {
   onSuccess: () => void;
